c9d2e72404d56a3e70ffa90335209f8138de90e4
courtney <rebocourtney@gmail.com>
2021-11-10 16:35:33 -0800

    fixed User model import statement

--- server/api/gateKeepingMiddleware.js
+++ server/api/gateKeepingMiddleware.js
@@ -1,24 +1,24 @@
-const { models: { User }} = require('../db/models/User.js')
+const { models: User } = require("../db/models/User.js");
 
 const requireToken = async (req, res, next) => {
   try {
-    const token = req.headers.authorization
-    const user = await User.findByToken(token)
-    req.user = user
+    const token = req.headers.authorization;
+    const user = await User.findByToken(token);
+    req.user = user;
   } catch (e) {
-      next(e)
+    next(e);
   }
-}
+};
 
 const isAdmin = (req, res, next) => {
   if (!req.user.isAdmin) {
-    return res.status(403).send("Permission denied")
+    return res.status(403).send("Permission denied");
   } else {
-    next()
+    next();
   }
-}
+};
 
 module.exports = {
   requireToken,
-  isAdmin
-}
+  isAdmin,
+};
