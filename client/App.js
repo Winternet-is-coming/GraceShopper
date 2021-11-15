e77758362e86410ff9845537cbb60099d8dd8aa5
courtney <rebocourtney@gmail.com>
2021-11-15 12:44:13 -0800

    fixed errors with routes, featuredsnacks, and home component

--- client/App.js
+++ client/App.js
@@ -1,14 +1,13 @@
-import React from 'react';
-import Navbar from './components/Navbar';
-import Routes from './Routes';
+import React from "react";
+import Navbar from "./components/Navbar";
+import Routes from "./Routes";
 
-import AllProducts from './components/AllProducts';
-import AllCards from './components/AllCards';
-import {BrowserRouter as Router, Link, Route, Switch} from 'react-router-dom';
-
+import AllProducts from "./components/AllProducts";
+import { BrowserRouter as Router, Link, Route, Switch } from "react-router-dom";
+import Footer from "./components/Footer";
 
 //Material UI
-import Grid from '@material-ui/core/Grid';
+import Grid from "@material-ui/core/Grid";
 
 const App = () => {
   return (
