1c5fd32252d7ca24946f5816c122728317ddb163
courtney <rebocourtney@gmail.com>
2021-11-16 09:20:51 -0800

    created initial backend logic for adding to cart

--- server/api/cart.js
+++ server/api/cart.js
@@ -2,6 +2,8 @@ const router = require("express").Router();
 const {
   models: { Order, Product, Order_Products, User },
 } = require("../db");
+const Sequelize = require("sequelize");
+const Op = Sequelize.Op;
 
 module.exports = router;
 
@@ -83,6 +85,49 @@ router.put("/:userId/:productId", async (req, res, next) => {
       await product.update({ quantity: req.body.data.newQuantity });
       console.log("done updating in db");
       res.json(product);
+    } else {
+      res.send("Access denied");
+    }
+  } catch (e) {
+    next(e);
+  }
+});
+
+// adding to cart in progress
+router.post("/:userId/:productId", async (req, res, next) => {
+  try {
+    const { id } = await User.findByToken(req.headers.authorization);
+
+    if (id === +req.params.userId) {
+      const product = await Order_Products.findOne({
+        include: {
+          model: Order,
+          where: {
+            userId: req.params.userId,
+          },
+          attributes: [],
+        },
+        where: {
+          productId: req.params.productId,
+        },
+      });
+
+      if (product) {
+        const newQuantity = product.quantity + 1;
+        await product.update({ quantity: newQuantity });
+        res.json(product);
+      } else {
+        const newEntry = await Order_Products.create({
+          orderId: req.params.userId,
+          productId: req.params.productId,
+          quantity: 1,
+        });
+        res.json(newEntry);
+      }
+
+      // insert a new row into Order_Products
+      // with matching userId and productId
+      // and quantity 1
     } else {
       res.send("Access denied");
     }
