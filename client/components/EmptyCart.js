8135e34cd27cb7f29830168e52f180bf1accf8f5
courtney <rebocourtney@gmail.com>
2021-11-16 08:05:25 -0800

    changed spelling of awesome on empty cart page

--- client/components/EmptyCart.js
+++ client/components/EmptyCart.js
@@ -1,23 +1,23 @@
-import React, {Component} from 'react';
-import Box from '@mui/material/Box';
-import {Button} from '@mui/material';
+import React, { Component } from "react";
+import Box from "@mui/material/Box";
+import { Button } from "@mui/material";
 export default function EmptyCart() {
-	return (
-		<div>
-			<Box sx={{width: '100%', display: 'flex', justifyContent: 'center'}}>
-				<h1>Oh no! Your cart looks very sad at the moment...</h1>
-			</Box>
-			<Box sx={{width: '100%', display: 'flex', justifyContent: 'center'}}>
-				<img
-					src="https://i1.wp.com/www.huratips.com/wp-content/uploads/2019/04/empty-cart.png?fit=603%2C288&ssl=1"
-					alt="empty cart"
-				/>
-			</Box>
-			<Box sx={{width: '100%', display: 'flex', justifyContent: 'center'}}>
-				<Button href="/products" variant="contained">
-					Checkout our awsome collection
-				</Button>
-			</Box>
-		</div>
-	);
+  return (
+    <div>
+      <Box sx={{ width: "100%", display: "flex", justifyContent: "center" }}>
+        <h1>Oh no! Your cart looks very sad at the moment...</h1>
+      </Box>
+      <Box sx={{ width: "100%", display: "flex", justifyContent: "center" }}>
+        <img
+          src="https://i1.wp.com/www.huratips.com/wp-content/uploads/2019/04/empty-cart.png?fit=603%2C288&ssl=1"
+          alt="empty cart"
+        />
+      </Box>
+      <Box sx={{ width: "100%", display: "flex", justifyContent: "center" }}>
+        <Button href="/products" variant="contained">
+          Checkout our awesome collection
+        </Button>
+      </Box>
+    </div>
+  );
 }
