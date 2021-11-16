c145d293c7bbe8dc623e2d6b3806858e03cf8eab
Grace Lin <gracelin3295@gmail.com>
2021-11-16 12:47:20 -0500

    designing confirmation page

--- client/components/Confirmation.js
+++ client/components/Confirmation.js
@@ -1,10 +1,18 @@
-import React, {Component} from 'react';
-import {connect} from 'react-redux';
+import React, { Component } from "react";
+import { connect } from "react-redux";
+
+//Material UI
+import Typography from "@material-ui/core/Typography";
+import Container from "@material-ui/core/Container";
 
 export default function Confirmation() {
-	return (
-		<div>
-			<h1>Thank you for your purchase!</h1>
-		</div>
-	);
+  return (
+    <Container alignitems="center" aligncontents="center">
+      <Container justifycontent="center">
+        <img src="https://itsugar.com/media/catalog/product/cache/9392bcc61c6b22792bba4834355aed07/a/s/asian_blind_box.png" />
+        <br />
+        <Typography variant="h2">Thank you for your purchase!</Typography>
+      </Container>
+    </Container>
+  );
 }
