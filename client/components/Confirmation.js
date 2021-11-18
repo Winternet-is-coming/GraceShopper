8fd786b05ff0d8e45f225449c8b97f25f3a56a94
Grace Lin <gracelin3295@gmail.com>
2021-11-17 23:05:11 -0500

    centered confirmation page

--- client/components/Confirmation.js
+++ client/components/Confirmation.js
@@ -4,14 +4,38 @@ import { connect } from "react-redux";
 //Material UI
 import Typography from "@material-ui/core/Typography";
 import Container from "@material-ui/core/Container";
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles({
+  thankYouImg: {
+    justifycontent: "center",
+    height: 400,
+    paddingLeft: 450,
+    paddingTop: 125,
+  },
+  imgContainer: {
+    width: "100%",
+  },
+  thankYouText: {
+    paddingLeft: 225,
+  },
+});
 
 export default function Confirmation() {
+  const classes = useStyles();
+
   return (
-    <Container alignitems="center" aligncontents="center">
-      <Container justifycontent="center">
-        <img src="https://itsugar.com/media/catalog/product/cache/9392bcc61c6b22792bba4834355aed07/a/s/asian_blind_box.png" />
+    <Container>
+      <Container justifycontent="center" className={classes.imgContainer}>
+        <img
+          src="https://c.tenor.com/WaQ9Avqbp38AAAAi/kikicat-kikiapp.gif"
+          className={classes.thankYouImg}
+        />
+
         <br />
-        <Typography variant="h2">Thank you for your purchase!</Typography>
+        <Typography variant="h2" className={classes.thankYouText}>
+          Your snacks are on the way!
+        </Typography>
       </Container>
     </Container>
   );
