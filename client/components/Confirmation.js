484113c3cdcc3344c1c5225f5247e1a8b539ed1a
Grace Lin <gracelin3295@gmail.com>
2021-11-17 23:32:13 -0500

    confirmation page completed

--- client/components/Confirmation.js
+++ client/components/Confirmation.js
@@ -4,7 +4,20 @@ import { connect } from "react-redux";
 //Material UI
 import Typography from "@material-ui/core/Typography";
 import Container from "@material-ui/core/Container";
+import Button from "@material-ui/core/Button";
 import { makeStyles } from "@material-ui/core/styles";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#ef9a9a",
+    },
+    secondary: {
+      main: "#f9c8d9",
+    },
+  },
+});
 
 const useStyles = makeStyles({
   thankYouImg: {
@@ -18,6 +31,9 @@ const useStyles = makeStyles({
   },
   thankYouText: {
     paddingLeft: 225,
+  },
+  buyMoreButton: {
+    paddingLeft: 460,
   },
 });
 
@@ -36,6 +52,14 @@ export default function Confirmation() {
         <Typography variant="h2" className={classes.thankYouText}>
           Your snacks are on the way!
         </Typography>
+        <ThemeProvider theme={theme}>
+          <div className={classes.buyMoreButton}>
+            <Button color="secondary" variant="contained" href="/products">
+              {" "}
+              Buy More Yummy Snacks
+            </Button>
+          </div>
+        </ThemeProvider>
       </Container>
     </Container>
   );
