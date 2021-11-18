ef0c9de6ddc0bef301c9f1b54d564d230a8bdc1c
courtney <rebocourtney@gmail.com>
2021-11-18 06:42:02 -0800

    added updated confirmation page

--- client/components/Confirmation.js
+++ client/components/Confirmation.js
@@ -4,14 +4,62 @@ import { connect } from "react-redux";
 //Material UI
 import Typography from "@material-ui/core/Typography";
 import Container from "@material-ui/core/Container";
+import Button from "@material-ui/core/Button";
+import { makeStyles } from "@material-ui/core/styles";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#ef9a9a",
+    },
+    secondary: {
+      main: "#f9c8d9",
+    },
+  },
+});
+
+const useStyles = makeStyles({
+  thankYouImg: {
+    justifycontent: "center",
+    height: 400,
+    paddingLeft: 450,
+    paddingTop: 125,
+  },
+  imgContainer: {
+    width: "100%",
+  },
+  thankYouText: {
+    paddingLeft: 225,
+  },
+  buyMoreButton: {
+    paddingLeft: 460,
+  },
+});
 
 export default function Confirmation() {
+  const classes = useStyles();
+
   return (
-    <Container alignitems="center" aligncontents="center">
-      <Container justifycontent="center">
-        <img src="https://itsugar.com/media/catalog/product/cache/9392bcc61c6b22792bba4834355aed07/a/s/asian_blind_box.png" />
+    <Container>
+      <Container justifycontent="center" className={classes.imgContainer}>
+        <img
+          src="https://c.tenor.com/WaQ9Avqbp38AAAAi/kikicat-kikiapp.gif"
+          className={classes.thankYouImg}
+        />
+
         <br />
-        <Typography variant="h2">Thank you for your purchase!</Typography>
+        <Typography variant="h2" className={classes.thankYouText}>
+          Your snacks are on the way!
+        </Typography>
+        <ThemeProvider theme={theme}>
+          <div className={classes.buyMoreButton}>
+            <Button color="secondary" variant="contained" href="/products">
+              {" "}
+              Buy More Yummy Snacks
+            </Button>
+          </div>
+        </ThemeProvider>
       </Container>
     </Container>
   );
